82162507e3dffbc4485ec72657e15b1ef5e3f366
Arilson Barcelos <arilson.barcelos@bemobi.com>
2025-08-14 19:37:39 -0300

    allow the EC2 hostname

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,13 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true,
+    port: 3000,
+    allowedHosts: [
+      'ec2-54-196-61-59.compute-1.amazonaws.com',
+      'localhost',
+      '127.0.0.1'
+    ]
+  }
 })
